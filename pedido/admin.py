14f32b6e3e7d85385e00d7be6f6762c9ef9d78c6
muriloeduardo199 <muriloeduardo1993@gmail.com>
2022-10-06 19:40:51 -0300

    update

--- pedido/admin.py
+++ pedido/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
-
+from . import models
 # Register your models here.
+class ItemPedidoInline(admin.TabularInline):
+    model= models.ItemPedido
+    extra=1
+
+class ProdutoAdmin(admin.ModelAdmin):
+    inlines= [
+        ItemPedidoInline
+    ]
+admin.site.register(models.Pedido, ProdutoAdmin)
+admin.site.register(models.ItemPedido)
